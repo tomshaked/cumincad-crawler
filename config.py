352139adc13786c77660b7e2dc71f98aba31d4fb
avi <tomshaked@gmail.com>
2018-07-23 08:56:37 +0300

    fix

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 ##  Database parameters
 DATABASE_CONFIG = {
     'host': 'localhost',
-    'dbname': 'cumincadCrawlerTest',
-    'user': 'root',
-    'password': '2911',
+    'dbname': '',
+    'user': '',
+    'password': '',
     'port': 3306
 }
 
 ##  Saved papers directory (On Windows OS, the SAVE_DIR path should use \ instead of /)
-SAVE_DIR = '/Users/avi 1/WorkSpace/CumInCAD-Crawler/content/'
+SAVE_DIR = ''
